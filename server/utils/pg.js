8962e75f12f8d79a071d35a0b9ae5bad5b2bba69
ckscks038038 <ckscks038038@gmail.com>
2022-12-28 18:22:24 +0800

    Update pg.js

--- server/utils/pg.js
+++ server/utils/pg.js
@@ -1,11 +1,20 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
+const {
+  POSTGRES_USER,
+  POSTGRES_HOST,
+  POSTGRES_DATABASE,
+  POSTGRES_PASSWORD,
+  POSTGRES_PORT,
+} = process.env;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '3.113.125.224',
-  database: 'yoyomantype',
-  password: 'postgres',
-  port: 5432,
+  user: POSTGRES_USER,
+  host: POSTGRES_HOST,
+  database: POSTGRES_DATABASE,
+  password: POSTGRES_PASSWORD,
+  port: POSTGRES_PORT,
 });
 
 pool
